c0d14a99bdd521a82dc91bd8e99274637d20d509
justinodevforever <jchitombi7@gmail.com>
2024-05-31 00:47:08 +0100

    adicionado atualizacoes222

--- src/v1/controllers/controllerReconfirmacao.js
+++ src/v1/controllers/controllerReconfirmacao.js
@@ -36,24 +36,24 @@ const createReconfirmacao = async (req, res) => {
       return;
     }
 
-    const response = await prisma.reconfirmacao.create({
-      data: {
-        valor: 2203,
-        rupe: 7728827,
-        fk_ano,
-        fk_curso,
-        fk_estudante,
-        fk_frequencia,
-        fk_semestre,
-        fk_user,
-      },
-    });
-    if (typeof response?.rupe === "bigint") {
-      response.rupe = response?.rupe?.toString();
-    }
-    res.status(201).json({ response: response, message: "sucess" });
+    // const response = await prisma.reconfirmacao.create({
+    //   data: {
+    //     valor: 2203,
+    //     rupe: 7728827,
+    //     fk_ano,
+    //     fk_curso,
+    //     fk_estudante,
+    //     fk_frequencia,
+    //     fk_semestre,
+    //     fk_user,
+    //   },
+    // });
+    // if (typeof response?.rupe === "bigint") {
+    //   response.rupe = response?.rupe?.toString();
+    // }
+    // res.status(201).json({ response: response, message: "sucess" });
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ message: "error" });
   }
 };
 
