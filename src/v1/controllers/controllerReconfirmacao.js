92048caab9307cbe00d2898f3564bd47403c3fca
justinodevforever <jchitombi7@gmail.com>
2024-05-31 00:29:51 +0100

    adicionado atualizacoes3

--- src/v1/controllers/controllerReconfirmacao.js
+++ src/v1/controllers/controllerReconfirmacao.js
@@ -53,7 +53,7 @@ const createReconfirmacao = async (req, res) => {
     // }
     // res.status(201).json({ response: response, message: "sucess" });
   } catch (error) {
-    res.json({ message: "error" });
+    res.json({ message: error.message });
   }
 };
 
